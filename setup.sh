12b052d277465200cf388471e39e513a835f18bd
Justin Searles <jsearles0619@gmail.com>
2015-06-09 21:38:53 -0400

    Allow the scheduler to do its job.

--- setup.sh
+++ setup.sh
@@ -2,16 +2,26 @@
 
 kubectl create -f zookeeper.json
 
+sleep 2m
+
 kubectl create -f zookeeper-service.json
 
 kubectl create -f storm-nimbus.json
+
+sleep 2m
 
 kubectl create -f storm-nimbus-service.json
 
 kubectl create -f storm-ui.json
 
+sleep 30
+
 kubectl create -f storm-ui-service.json
 
 kubectl create -f storm-worker-controller.json
 
+sleep 30
+
 kubectl create -f storm-worker-service.json
+
+kubectl get pods,services,rc

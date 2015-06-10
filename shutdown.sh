f7f701a168484c3faf90aa10d9b5d6486baf01b2
Justin Searles <jsearles0619@gmail.com>
2015-06-09 20:41:06 -0400

    Shutdown script.

---
+++ shutdown.sh
@@ -0,0 +1,15 @@
+kubectl delete -f zookeeper.json
+
+kubectl delete -f zookeeper-service.json
+
+kubectl delete -f storm-nimbus.json
+
+kubectl delete -f storm-nimbus-service.json
+
+kubectl delete -f storm-ui.json
+
+kubectl delete -f storm-ui-service.json
+
+kubectl delete -f storm-worker-controller.json
+
+kubectl delete -f storm-worker-service.json
